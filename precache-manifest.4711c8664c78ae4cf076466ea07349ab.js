c160048e0f85f8fa58ff64b376b4f3401cbcbd63
Chris Wiles <chriswiles87@gmail.com>
2020-03-12 11:12:14 -0500

    Updates

---
+++ precache-manifest.4711c8664c78ae4cf076466ea07349ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a38f3f5b40d0de2cb351dd7157f9e8b",
+    "url": "/TriviaChallenge/index.html"
+  },
+  {
+    "revision": "7b13cf2e3c6a12ae9757",
+    "url": "/TriviaChallenge/static/js/0.107fb7c4.chunk.js"
+  },
+  {
+    "revision": "fd4c56e72174d94f4232",
+    "url": "/TriviaChallenge/static/js/3.3ce197f5.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/TriviaChallenge/static/js/3.3ce197f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e95e67a05433b1e8079",
+    "url": "/TriviaChallenge/static/js/4.3a3365ae.chunk.js"
+  },
+  {
+    "revision": "28507dfd278c742f2c36",
+    "url": "/TriviaChallenge/static/js/5.2772ea09.chunk.js"
+  },
+  {
+    "revision": "04fb7a1db0b2946f87bd",
+    "url": "/TriviaChallenge/static/js/6.48afdefb.chunk.js"
+  },
+  {
+    "revision": "de155e61e5194d984af5",
+    "url": "/TriviaChallenge/static/js/7.9601e6d6.chunk.js"
+  },
+  {
+    "revision": "60ccee46e0b820ae06bc",
+    "url": "/TriviaChallenge/static/js/8.5b8a47a5.chunk.js"
+  },
+  {
+    "revision": "6d76bf969cb77a808536",
+    "url": "/TriviaChallenge/static/js/main.c7d82360.chunk.js"
+  },
+  {
+    "revision": "ec0ec2b4b13ed5f67235",
+    "url": "/TriviaChallenge/static/js/runtime-main.48648bd6.js"
+  }
+]);
---
+++ static/js/8.5b8a47a5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpg2i-code-challenge-web-template"]=this["webpackJsonpg2i-code-challenge-web-template"]||[]).push([[8],{231:function(e,t,a){"use strict";a.r(t);var n=a(0),c=a.n(n),l=a(54);t.default=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(l.a,{gutterBottom:!0,variant:"h4"},"Statistics"))}}}]);
+//# sourceMappingURL=8.5b8a47a5.chunk.js.map
